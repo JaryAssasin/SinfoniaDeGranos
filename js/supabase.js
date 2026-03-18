53d83ac456c433aaaf1a59da036b36f981b9d83e
JaryAssasin <Jaroloco00@gmail.com>
2026-03-18 15:07:39 -0600

    cambio key anon

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://ibftxwnyyxxvouymdcxu.supabase.co";
-const supabaseKey = "sb_publishable_rcbz7jan-wV8lwwwFCLrRw_B4otPcD7";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZnR4d255eXh4dm91eW1kY3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNTg1ODgsImV4cCI6MjA4ODgzNDU4OH0.pC6q8GP14udTvKHWILwnOJwGmE6F7oZPWXdrYolM-78";
 
 const supabase = window.supabase.createClient(
     supabaseUrl,
